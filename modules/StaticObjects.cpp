0a6f959d3be95eb34ba256bd4fa9dac6a20b2d01
tsiverakis <tsibers@gmail.com>
2025-12-01 20:56:49 +0200

    ebala to possition sto static

---
+++ modules/StaticObjects.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+class Position
+{
+private:
+    int x, y;
+
+public:
+    Position()
+    {
+        x = -1;
+        y = -1;
+    }
+
+    Position(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
+    void setX(int x)
+    {
+        this->x = x;
+    }
+
+    void setY(int y)
+    {
+        this->y = y;
+    }
+
+    int getX() const
+    {
+        return this->x;
+    }
+
+    int getY() const
+    {
+        return this->y;
+    }
+};
