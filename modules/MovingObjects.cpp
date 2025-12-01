5d5e4925d44e364ee3b2bf47ebee227e1ad3b003
Mousouris Charalampos <sdi2400130@di.uoa.gr>
2025-12-01 20:45:52 +0200

    Move

---
+++ modules/MovingObjects.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Position {
+    private:
+        int x, y;
+    public:
+        Position() {
+            x = -1;
+            y = - 1;
+        }
+
+        Position(int x, int y) {
+            this->x = x;
+            this->y = y;
+        }
+
+        void setX(int x) {
+            this->x = x;
+        }
+
+        void setY(int y) {
+            this->y  = y;
+        }
+
+        int getX() const {
+            return this->x;
+        }
+
+        int getY() const {
+            return this->y;
+        }
+}
